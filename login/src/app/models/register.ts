d09b7c9689df715db02025ec785bf61562191330
Umesh Kumar Mandal <umesh.mandal619@gmail.com>
2020-07-05 22:43:30 +0545

    Create register.ts

---
+++ login/src/app/models/register.ts
@@ -0,0 +1,10 @@
+export class Register {  
+    UserName:string;  
+    LoginName:string;  
+    Password:string;  
+    Email:string;  
+    ContactNo:string;  
+    Address:string;
+    Status: string;  
+    IsApproved: string;
+}  
