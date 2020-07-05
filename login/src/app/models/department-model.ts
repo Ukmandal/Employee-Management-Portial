16c716101428c640d91018313950639610ed6b60
Umesh Kumar Mandal <umesh.mandal619@gmail.com>
2020-07-05 22:46:13 +0545

    Add files via upload

---
+++ login/src/app/models/department-model.ts
@@ -0,0 +1,4 @@
+export class Department {
+    DepartmentID: number;
+    DepartmentName: string;
+}
---
+++ login/src/app/models/employee-model.ts
@@ -0,0 +1,11 @@
+export class Employee {
+    EmployeeID: number;
+    EmployeeName: string;
+    Department: string;
+    MailID: string;
+    DOJ: Date;
+    Salary: number;
+    Age: number;
+    Address: string;
+    Phone: number;
+}
