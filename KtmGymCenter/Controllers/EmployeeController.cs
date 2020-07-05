a9e00ba824c25efee9ef9b0b3220188651c71429
Umesh Kumar Mandal <umesh.mandal619@gmail.com>
2020-07-05 22:59:29 +0545

    Add files via upload

---
+++ KtmGymCenter/Controllers/EmployeeController.cs
@@ -0,0 +1,141 @@
+﻿using KtmGymCenter.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace KtmGymCenter.Controllers
+{
+    [RoutePrefix("Api/Employee")]
+    public class EmployeeController : ApiController
+    {
+        private EmployeeEntities objEntity = new EmployeeEntities();
+
+        [HttpGet]
+        [Route("AllEmployees")]
+        public IQueryable<Employee> GetEmployee()
+        {
+            try
+            {
+                return objEntity.Employees;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        [HttpGet]
+        [Route("Department")]
+        public IQueryable<Department> GetDepartment()
+        {
+            try
+            {
+                return objEntity.Departments;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        [HttpGet]
+        [Route("GetEmployeesById/{employeeId}")]
+        public IHttpActionResult GetEmployeeById(string employeeId)
+        {
+            Employee objEmp = new Employee();
+            int ID = Convert.ToInt32(employeeId);
+            try
+            {
+                objEmp = objEntity.Employees.Find(ID);
+                if (objEmp == null)
+                {
+                    return NotFound();
+                }
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return Ok(objEmp);
+        }
+
+        [HttpPost]
+        [Route("InsertEmployees")]
+        public IHttpActionResult PostEmployee(Employee data)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                objEntity.Employees.Add(data);
+                objEntity.SaveChanges();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return Ok(data);
+        }
+
+        [HttpPut]
+        [Route("UpdateEmployees")]
+        public IHttpActionResult PutEmployee(Employee employee)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                Employee objEmp = new Employee();
+                objEmp = objEntity.Employees.Find(employee.EmployeeID);
+                if (objEmp != null)
+                {
+                    objEmp.EmployeeName = employee.EmployeeName;
+                    objEmp.Department = employee.Department;
+                    objEmp.MailID = employee.MailID;
+                    objEmp.DOJ = employee.DOJ;
+                    objEmp.Address = employee.Address;
+                    objEmp.Phone = employee.Phone;
+                    objEmp.Salary = employee.Salary;
+                    objEmp.Age = employee.Age;
+
+                }
+                int i = this.objEntity.SaveChanges();
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return Ok(employee);
+        }
+
+        [HttpDelete]
+        [Route("DeleteEmployees")]
+        public IHttpActionResult DeleteEmployee(int id)
+        {
+            //int empId = Convert.ToInt32(id);  
+            Employee employee = objEntity.Employees.Find(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            objEntity.Employees.Remove(employee);
+            objEntity.SaveChanges();
+
+            return Ok(employee);
+        }
+    }
+}
---
+++ KtmGymCenter/Controllers/LoginController.cs
@@ -0,0 +1,60 @@
+﻿using KtmGymCenter.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace KtmGymCenter.Controllers
+{
+    public class LoginController : ApiController
+    {
+        //For user login   
+        [Route("Api/Login/UserLogin")]
+        [HttpPost]
+        public Response Login(Login Lg)
+        {
+            EmployeeEntities DB = new EmployeeEntities();
+            var Obj = DB.Usp_Login(Lg.UserName, Lg.Password).ToList<Usp_Login_Result>().FirstOrDefault();
+            //if (Obj.Status == 0)
+            //    return new Response { Status = "Invalid", Message = "Invalid User." };
+            //if (Obj.Status == -1)
+            //    return new Response { Status = "Inactive", Message = "User Inactive." };
+            //else
+            return new Response { Status = "Success", Message = Lg.UserName };
+        }
+        //For new user Registration  
+        [Route("Api/Login/createcontact")]
+        [HttpPost]
+        public object createcontact(Registration Lvm)
+        {
+            try
+            {
+                EmployeeEntities db = new EmployeeEntities();
+                Employeemaster Em = new Employeemaster();
+                if (Em.UserId == 0)
+                {
+                    Em.UserName = Lvm.UserName;
+                    Em.LoginName = Lvm.LoginName;
+                    Em.Password = Lvm.Password;
+                    Em.Email = Lvm.Email;
+                    Em.ContactNo = Lvm.ContactNo;
+                    Em.Address = Lvm.Address;
+                    Em.IsApporved = Lvm.IsApporved;
+                    Em.Status = Lvm.Status;
+                    db.Employeemasters.Add(Em);
+                    db.SaveChanges();
+                    return new Response
+                    { Status = "Success", Message = "SuccessFully Saved." };
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return new Response
+            { Status = "Error", Message = "Invalid Data." };
+        }
+    }
+}
