4c51b12195457ee1a844253992e18821c683e05a
Umesh Kumar Mandal <umesh.mandal619@gmail.com>
2020-07-05 23:01:05 +0545

    Add files via upload

---
+++ KtmGymCenter/Models/Response.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KtmGymCenter.Models
+{
+    public class Response
+    {
+        public string Status { set; get; }
+        public string Message { set; get; }
+    }
+}
