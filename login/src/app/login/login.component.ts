27733e73a63f71c9f3484fc699c994ba10d0d32c
Umesh Kumar Mandal <umesh.mandal619@gmail.com>
2020-07-07 19:53:40 +0545

    Add files via upload

--- login/src/app/login/login.component.ts
+++ login/src/app/login/login.component.ts
@@ -1,30 +1,50 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, FormBuilder, Validators, FormGroup } from '@angular/forms';
 import { LoginService } from '../services/login.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Register } from '../models/register';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-
+  password: boolean = false;
+  loginForm: FormGroup;
   model: any = {};
   errorMessage: string;
-  constructor(private router: Router, private LoginService: LoginService) { }
+  constructor(private router: Router, 
+    private LoginService: LoginService,
+    private fb: FormBuilder,
+    public snackBar: MatSnackBar,
+ ) { }
 
   ngOnInit() {
+    this.loginForm = this.fb.group({
+      UserName: [null, [Validators.required]],
+      Password: [null, [Validators.required, Validators.minLength(4), Validators.maxLength(30)]],
+    });
+
     sessionStorage.removeItem('UserName');
     sessionStorage.clear();
+  }
+
+  togglePassword() {
+    this.password = !this.password;
   }
 
   login() {
     this.LoginService.Login(this.model).subscribe(
       data => {
-        console.log(this.model);
         if (data.Status == "Success") {
-          console.log(this.model);
           this.router.navigate(['/Dashboard']);
+          let snackBarRef = this.snackBar.open('loggedin Successfully!',
+      'Got it!', {
+        duration: 3000,
+        verticalPosition: 'top',
+        horizontalPosition: 'right',
+      });
         }
         else {
           this.errorMessage = data.Message;
@@ -34,8 +54,4 @@ export class LoginComponent {
         this.errorMessage = error.message;
       });
   };
-
-  register() {
-    this.router.navigate(['/register']);
-  }
 }     
