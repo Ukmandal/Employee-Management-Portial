20999ad1e2538d3ea645996f7d334dd6bc2f659e
Umesh Kumar Mandal <umesh.mandal619@gmail.com>
2020-07-07 19:56:59 +0545

    Create delete-emp.component.ts

---
+++ login/src/app/employee/delete-emp/delete-emp.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialogRef } from '@angular/material/dialog';
+
+@Component({
+  template: `
+  <div style="text-align: center;">
+  <h1 mat-dialog-title>Confirm</h1><hr>
+  <div mat-dialog-content>{{confirmMessage}}</div>
+  <div mat-dialog-actions style="justify-content: center;">
+    <button mat-button style="color: #fff;background-color: #153961;" 
+    (click)="dialogRef.close(true)">Confirm</button>
+    <button  mat-button (click)="dialogRef.close(false)">Cancel</button>
+  </div>
+  </div>
+  `,
+})
+export class DeleteEmpComponent {
+  constructor(
+    public snackBar: MatSnackBar,
+    public dialogRef: MatDialogRef<DeleteEmpComponent>) { }
+  confirmMessage: string = "Are you sure you want to delete?";
+}
